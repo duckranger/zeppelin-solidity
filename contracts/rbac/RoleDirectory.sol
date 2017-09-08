5e73d8ad1d0c6d54a694043e1b149589dbd6f8d9
nimo <nimo@ipayroll.co.nz>
2017-09-08 17:37:48 +1200

    Added test

--- contracts/rbac/RoleDirectory.sol
+++ contracts/rbac/RoleDirectory.sol
@@ -3,30 +3,101 @@ pragma solidity ^0.4.11;
 import '../ownership/Ownable.sol';
 
 /**
- * @title SecurityControl
- * @dev A simple security centre to define roles and assign them to addresses
- * (users)
+ * @title RoleDirecory
+ * @dev RoleDirecory is a simple directory that enables its user to define roles
+ * and assign them to addresses (users)
  *
- * A role is constructed of a unique name (within the SecurityControl) and a weight.
+ * A role is constructed of a unique name (within the RoleDirecory) and a weight.
  * The weight of the role can be used by developers wanting to create a role
- * hierarchy. When the hasRole function is used, it will check that the clled has
+ * hierarchy. When the hasRole function is used, it will check that the caller has
  * either the exact role name, or a role that carries more weight.
+ *
+ * In a non-hierarchical situation, all roles can be created with the same weight.
  */
 contract RoleDirectory is Ownable {
 
+  // A definition of a single Role in the system
   struct Role {
-    string name;
-    uint8 weight;
+    string name;   //Role's name - must be unique
+    uint8 weight;  //Role's weight
   }
 
+  // All the roles available in the system, mapped by Role.name
   mapping(string=>Role) private systemRoles;
+
+  // Mapping of a user (address) to the set of Roles assigned to them
   mapping(address=>Role[]) private userRoles;
 
-  function addRoleToUser(address _user, string _role) onlyOwner {
-    require(roleExists(_role));
-    userRoles[_user].push(systemRoles[_role]);
+  // @dev Owner of the RoleDirectory may add new roles in the system.
+  // The new role has to have a unique name
+  // @param _name - the name of the new role
+  // @param _weight - weight to the role. This is used for hierarchy
+  function addSystemRole(string _name, uint8 _weight) onlyOwner {
+      require (!roleExists(_name));
+      systemRoles[_name] = Role({
+        name:_name,
+        weight: _weight
+      });
   }
 
+  // @dev A shorthand function to add a role with no weight. This can be
+  // used for systems where an exact role is required, with no hierarchy.
+  // @param _name - the name of the new role
+  function addZeroWeightSystemRole(string _name) onlyOwner {
+    addSystemRole(_name,0);
+  }
+
+  function changeRoleWeight(string _name, uint8 _newWeight) {
+    require (roleExists(_name));
+    systemRoles[_name].weight = _newWeight;
+  }
+
+  // @dev Adds a role to a user
+  // If the user already has the role - it will not be added again.
+  // @param _user - the address to add the role to
+  // @param _role - the name of the role to add to the user
+  function addRoleToUser(address _user, string _role) onlyOwner {
+    require(roleExists(_role));
+    if (!userInRole(_user, _role)) {
+      userRoles[_user].push(systemRoles[_role]);
+    }
+  }
+
+  // @dev remove a role from user
+  // @param _user - the address to remove the role from
+  // @param _role - the name of the role to remove
+  function removeRoleFromUser(address _user, string _role) onlyOwner {
+    require(roleExists(_role));
+    var numRolesForUser = userRoles[_user].length;
+    if (numRolesForUser!=0) {
+      for (uint i = 0 ; i < userRoles[_user].length; i++) {
+        if (sha3(userRoles[_user][i].name) == sha3(_role)) {
+          userRoles[_user][i] = userRoles[_user][numRolesForUser-1];
+          userRoles[_user].length--;
+        }
+      }
+    }
+  }
+
+  // @dev Checks whether the user has a specific role
+  // @param _user - the address to check the role for
+  // @param _roleName - the role to check for
+  // @return bool - whether the user has the role assigned to them or not
+  function userInExactRole(address _user, string _roleName) constant returns (bool) {
+    for (uint i = 0 ; i < userRoles[_user].length; i++) {
+      if (sha3(userRoles[_user][i].name) == sha3(_roleName)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // @dev Checks whether a specific user has a role assigned, or - they have
+  // a higher weight role assigned to them.
+  // @param _user - the address to check the role for
+  // @param _roleName - the role to check for
+  // @return bool - whether the user has the either the exact role or a higher weight
+  // role assigned to them or not
   function userInRole(address _user, string _roleName) constant returns (bool) {
     require(roleExists(_roleName));
     for (uint i = 0 ; i < userRoles[_user].length; i++) {
@@ -38,24 +109,9 @@ contract RoleDirectory is Ownable {
     return false;
   }
 
-  //Used by the owner only to add a new role in the system.
-  //The new role has to have a unique name.
-  //@param _name - the name of the new role
-  //@param _weight - weight to the role. This is used for hierarchy
-  function addSystemRole(string _name, uint8 _weight) onlyOwner {
-      require (!roleExists(_name));
-      systemRoles[_name] = Role({
-        name:_name,
-        weight: _weight
-      });
-  }
-
-  // A shorthand function to add a role with the lowest weight. This can be
-  // used for systems where an exact role is required, with no hierarchy.
-  function addSystemRole(string _name) onlyOwner {
-    addSystemRole(_name,0);
-  }
-
+  // @dev Checks whether a role exists in the system.
+  // @param _name - the name to check.
+  // @return bool - whether the role is registered in the system or not
   function roleExists(string _name) constant returns (bool) {
     require(bytes(_name).length > 0);
     return bytes(systemRoles[_name].name).length!=0;
---
+++ test/RoleDirectory.js
@@ -0,0 +1,121 @@
+const assertJump = require('./helpers/assertJump');
+var RoleDirectory = artifacts.require('../contracts/rbac/RoleDirectory.sol');
+
+contract('RoleDirectory', function(accounts) {
+  let roleDirectory;
+
+  beforeEach(async function() {
+    roleDirectory = await RoleDirectory.new();
+    await roleDirectory.addSystemRole('role_1',5);
+  });
+
+  it("should store new role", async function() {
+    let result = await roleDirectory.roleExists('role_1');
+    assert.isTrue(result);
+  });
+
+  it("should not allow double role", async function() {
+    try {
+      let result = await roleDirectory.addSystemRole('role_1',0);
+      assert.fail('should have thrown before');
+    } catch(error) {
+      assertJump(error);
+    }
+  });
+
+  it("should add zero weight roles", async function() {
+    await roleDirectory.addZeroWeightSystemRole('role_2');
+    let result = await roleDirectory.roleExists('role_2');
+    assert.isTrue(result);
+  });
+
+  it("should not have a role that was not added", async function() {
+    let result = await roleDirectory.roleExists('role_2');
+    assert.isFalse(result);
+  });
+
+  it("should be able to contain multiple roles", async function() {
+    await roleDirectory.addSystemRole('role_2',15);
+    await roleDirectory.addSystemRole('role_3',25);
+    let role1Exists = await roleDirectory.roleExists('role_1');
+    let role2Exists = await roleDirectory.roleExists('role_2');
+    let role3Exists = await roleDirectory.roleExists('role_3');
+    assert.isTrue(role1Exists && role2Exists && role3Exists);
+  });
+
+  it("should assign a role to a user", async function() {
+    await roleDirectory.addRoleToUser(1,'role_1');
+    let userHasRole = await roleDirectory.userInRole(1,'role_1');
+    assert.isTrue(userHasRole);
+  });
+
+  it("should not assign an extra role to user", async function() {
+    await roleDirectory.addSystemRole('role_2',15);
+    await roleDirectory.addRoleToUser(1,'role_1');
+    let userHasRole1 = await roleDirectory.userInRole(1,'role_1');
+    let userHasRole2 = await roleDirectory.userInRole(1,'role_2');
+    assert.isTrue(userHasRole1);
+    assert.isFalse(userHasRole2);
+  });
+
+  it("should allow users to have multiple roles", async function() {
+    await roleDirectory.addSystemRole('role_2',5);
+    await roleDirectory.addRoleToUser(1,'role_1');
+    await roleDirectory.addRoleToUser(1,'role_2');
+    let userHasRole1 = await roleDirectory.userInRole(1,'role_1');
+    let userHasRole2 = await roleDirectory.userInRole(1,'role_2');
+    assert.isTrue(userHasRole1);
+    assert.isTrue(userHasRole2);
+  });
+
+  it("should find exact role", async function() {
+    await roleDirectory.addRoleToUser(1,'role_1');
+    let userHasRole1 = await roleDirectory.userInExactRole(1,'role_1');
+    assert.isTrue(userHasRole1);
+  });
+
+  it("should adhere to hierarchy", async function() {
+    await roleDirectory.addSystemRole('role_2',15);
+    await roleDirectory.addRoleToUser(1,'role_2');
+    let userHasRole1 = await roleDirectory.userInRole(1,'role_1');
+    assert.isTrue(userHasRole1);
+  });
+
+  it("should be able to remove role from a user", async function() {
+    await roleDirectory.addSystemRole('role_2',15);
+    await roleDirectory.addRoleToUser(1,'role_1');
+    await roleDirectory.addRoleToUser(1,'role_2');
+    let userHasRole1 = await roleDirectory.userInExactRole(1,'role_1');
+    let userHasRole2 = await roleDirectory.userInExactRole(1,'role_2');
+
+    // assert user has both roles before removing any
+    assert.isTrue(userHasRole1);
+    assert.isTrue(userHasRole2);
+
+    // remove role2, assert it is gone, and that the user still has role1
+    await roleDirectory.removeRoleFromUser(1,'role_2');
+    userHasRole2 = await roleDirectory.userInExactRole(1,'role_2');
+    assert.isFalse(userHasRole2);
+    userHasRole1 = await roleDirectory.userInExactRole(1,'role_1');
+    assert.isTrue(userHasRole1);
+
+    // remove role1, assert it is gone.
+    await roleDirectory.removeRoleFromUser(1,'role_1');
+    userHasRole1 = await roleDirectory.userInExactRole(1,'role_1');
+    assert.isFalse(userHasRole1);
+  });
+
+  it("should be able to change a role weight", async function() {
+    await roleDirectory.addSystemRole('role_2',25);
+    await roleDirectory.addRoleToUser(1,'role_2');
+    let userHasRole1 = await roleDirectory.userInRole(1,'role_1');
+    assert.isTrue(userHasRole1);
+
+    // now - change role_2 weight to below role 1 weight,
+    // if this works, then userInRole('role_1') will not be true.
+    await roleDirectory.changeRoleWeight('role_2',1);
+    userHasRole1 = await roleDirectory.userInRole(1,'role_1');
+    assert.isTrue(userHasRole1);
+  });
+
+});
